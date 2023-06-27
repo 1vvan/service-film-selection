cac194a60f73ead37f773b6e9342e05d7f99dd47
Ivan <123951392+1vvan@users.noreply.github.com>
2023-06-28 01:17:51 +0300

    Fix

--- src/components/VideoBackgroud/VideoBackground.jsx
+++ src/components/VideoBackgroud/VideoBackground.jsx
@@ -1,20 +1,69 @@
-import React from 'react';
-import videoBg from '../../assets/videoBg.mp4'
-import "./VideoBackground.css"
+import React, { useEffect, useRef } from "react";
+import videoBg from "../../assets/videoBg.mp4";
+import "./VideoBackground.css";
 
 const VideoBackground = () => {
-    return (
-      <>
-        <video
-          className="video_bg"
-          src={videoBg}
-          playsInline
-          autoPlay
-          loop
-          muted
-        ></video>
-      </>
-    );
-}
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const handlePlay = () => {
+      if (
+        videoRef.current &&
+        videoRef.current.paused &&
+        videoRef.current.readyState === 4
+      ) {
+        videoRef.current.play();
+      }
+    };
+
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        videoRef.current.pause();
+      } else {
+        handlePlay();
+      }
+    };
+
+    const handleBatteryChange = () => {
+      if (navigator.getBattery) {
+        navigator.getBattery().then((battery) => {
+          if (battery.charging || battery.level > 0.5) {
+            handlePlay();
+          } else {
+            videoRef.current.pause();
+          }
+        });
+      } else {
+        handlePlay();
+      }
+    };
+
+    document.addEventListener("visibilitychange", handleVisibilityChange);
+    window.addEventListener("blur", handlePlay);
+    window.addEventListener("focus", handlePlay);
+    window.addEventListener("batterychange", handleBatteryChange);
+
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange);
+      window.removeEventListener("blur", handlePlay);
+      window.removeEventListener("focus", handlePlay);
+      window.removeEventListener("batterychange", handleBatteryChange);
+    };
+  }, []);
+  return (
+    <>
+      <video
+        ref={videoRef}
+        className="video_bg"
+        src={videoBg}
+        autoPlay
+        loop
+        muted
+        playsInline
+        preload="auto"
+      ></video>
+    </>
+  );
+};
 
 export default VideoBackground;
